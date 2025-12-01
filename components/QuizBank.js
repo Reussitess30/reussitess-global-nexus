d907b80c9f75f8fa9e6845ec8467d24c2c24a21b
Reussitess© <influenceur@reussitess.fr>
2025-12-01 10:25:50 -0400

    Create QuizBank.js

---
+++ components/QuizBank.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    question: "Quel pays détient le plus de sites UNESCO au monde ?",
+    options: ["France", "Italie", "Brésil"],
+    answer: "Italie"
+  },
+  {
+    question: "Quel est le surnom de la Guadeloupe ?",
+    options: ["Terre de Champions", "Amazon Paradise", "Culture Queen"],
+    answer: "Terre de Champions"
+  }
+];
