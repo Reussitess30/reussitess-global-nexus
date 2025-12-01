fc8b04f4b32916546395687f5f623f5be4addf2b
Reussitess© <influenceur@reussitess.fr>
2025-12-01 10:24:14 -0400

    Create Badges.js

---
+++ components/Badges.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 'globetrotter', label: 'Globetrotter', emoji: '🌍', condition: 'A posé 10 questions' },
+  { id: 'culture-star', label: 'Culture Star', emoji: '🏆', condition: 'A réussi un quiz' },
+  { id: 'amazon-master', label: 'Amazon Master', emoji: '🛍️', condition: 'A demandé 5 fois sur les boutiques' }
+];
