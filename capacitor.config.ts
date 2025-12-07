02f9c31b405eea464704f199702f043abcddc1d4
Reussitess30 <influenceur@reussitess.fr>
2025-12-06 21:28:22 -0400

    🚀 GLOBAL NEXUS ULTIMATE: Monétisation + APK + Bot Pro

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Reussitess®971',
+  webDir: 'public'
+};
+
+export default config;
